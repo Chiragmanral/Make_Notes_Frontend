212fd5dad2f304783e7521e96ce37daeae6a5db4
Chirag Singh Manral <chiragmanral2003@gmail.com>
2025-06-10 16:16:59 +0530

    update in code

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  generateNotes : boolean = true;
+  viewNotes : boolean = false;
+
+  getGenerateNotes() {
+    this.generateNotes = true;
+    this.viewNotes = false;
+  }
+
+  getViewNotes() {
+    this.generateNotes = false;
+    this.viewNotes = true;
+  }
+}
